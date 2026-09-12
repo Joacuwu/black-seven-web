e5070f5cb067399dab46a136b1b6c7fd521216ea
Joacuwu <joa.bobbio@gmail.com>
2026-09-12 00:09:38 -0300

    Fix: tipos de id corregidos en la pagina de producto

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import ProductGrid from "../components/ProductGrid";
 import CartDrawer from "../components/CartDrawer";
 import Footer from "../components/footer";
+import Link from "next/link";
 
 export default function Home() {
   return (
