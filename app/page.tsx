e681904a7a531de7f54af62f807fb7ecfeb14ec6
Joacuwu <joa.bobbio@gmail.com>
2026-09-11 23:49:06 -0300

    Feat: agregar Footer componente

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Hero from "../components/Hero";
 import ProductGrid from "../components/ProductGrid";
 import CartDrawer from "../components/CartDrawer";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
